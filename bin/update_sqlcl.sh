0db1d7405e9e621e06230000cd97a5433604e863
Gunther Weißenbäck <98875505+guntherweissenbaeck@users.noreply.github.com>
2025-05-28 08:11:01 +0200

    Update update_sqlcl.sh

--- bin/update_sqlcl.sh
+++ bin/update_sqlcl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Download the latest version of SQLcl to /home/gwe/Downloads
-wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip -O /home/gwe/Downloads/sqlcl-latest.zip
+wget https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-latest.zip -O ~/Downloads/sqlcl-latest.zip
 
 # Unzip the downloaded file to /home/gwe/.opt/sqlcl
-unzip -o /home/gwe/Downloads/sqlcl-latest.zip -d /home/gwe/.opt/sqlcl
+unzip -o ~/Downloads/sqlcl-latest.zip -d ~/.opt/sqlcl
 
 # Remove the downloaded zip file
-rm /home/gwe/Downloads/sqlcl-latest.zip
+rm ~/Downloads/sqlcl-latest.zip
 
